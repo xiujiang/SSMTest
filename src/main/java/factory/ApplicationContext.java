2486dee4f17af0fe198f5cf2fb918ec2eb74783d
xiujiang <liuxiujiang@youxin.com>
2018-12-10 14:30:05 +0800

    Initial commit

---
+++ src/main/java/factory/ApplicationContext.java
@@ -0,0 +1,21 @@
+package factory;/*
+ * Copyright 2017 - 数多科技
+ *
+ * 北京数多信息科技有限公司 本公司保留所有下述内容的权利。
+ * 本内容为保密信息，仅限本公司内部使用。
+ * 非经本公司书面许可，任何人不得外泄或用于其他目的。
+ */
+
+
+/**
+ * @author liuxiujiang
+ * @version 1.0
+ * @datetime 2018/12/10
+ * @since 1.8
+ */
+public class ApplicationContext implements BeanFactory{
+    @Override
+    public Object getBean(String beanId) {
+        return null;
+    }
+}
